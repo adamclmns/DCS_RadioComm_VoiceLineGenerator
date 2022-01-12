5483f2a61a19e3a3ba936ff08efd1bc9bffc215d
AdamClmns <adam@adamclmns.com>
2022-01-12 09:15:52 -0600

    Formatting, naming.

--- dcs_speech_generator.py
+++ dcs_speech_generator.py
@@ -1,65 +1,84 @@
-import pyttsx3, getpass, os, sys, argparse
-from pydub import AudioSegment
-from pydub import generators
-from pydub import silence
+import pyttsx3
+import getpass
+import os
+import sys
+import argparse
+from pydub import AudioSegment, generators, silence
 
-## CONFIGURATION - This is per your Local Machine installation of ffmpeg.
-ffmpegHome = "C:\\ffmpeg-2022-01-10-git-f37e66b393-full_build\\bin" 
-AudioSegment.converter = ffmpegHome+"\\ffmpeg.exe"
-AudioSegment.ffmpeg = ffmpegHome+"\\ffmpeg.exe"
-AudioSegment.ffprobe = ffmpegHome+"\\ffprobe.exe"
 
-## Audio files for 'click in' and 'click out'
-IN_WAV = "In.wav"
-OUT_WAV = "Out.wav"
+# CONFIGURATION - This is per your Local Machine installation of ffmpeg. Try running this in IDLE if it fails. If you cannot read the env variable, you might try rebooting.
+_ffmpegHome = os.getenv("FFMPEG_HOME")
+AudioSegment.converter = _ffmpegHome+"\\ffmpeg.exe"
+AudioSegment.ffmpeg = _ffmpegHome+"\\ffmpeg.exe"
+AudioSegment.ffprobe = _ffmpegHome+"\\ffprobe.exe"
+# This voice is on 99% of all Windows 10/11 systems
+_default_voiceID = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0"
+# Audio files for 'click in' and 'click out'
+_IN_WAV = "In.wav"
+_OUT_WAV = "Out.wav"
 
-## Convinient lambdas found on SO
-trim_leading_silence: AudioSegment = lambda x: x[silence.detect_leading_silence(x) :]
-trim_trailing_silence: AudioSegment = lambda x: trim_leading_silence(x.reverse()).reverse()
-strip_silence: AudioSegment = lambda x: trim_trailing_silence(trim_leading_silence(x))
+# Convinient lambdas found on SO
+_trim_leading_silence: AudioSegment = lambda x: x[silence.detect_leading_silence(
+    x):]
+_trim_trailing_silence: AudioSegment = lambda x: _trim_leading_silence(
+    x.reverse()).reverse()
+_strip_silence: AudioSegment = lambda x: _trim_trailing_silence(
+    _trim_leading_silence(x))
 
-default_voiceID = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0"
-## Adds a couple filters, and adds static, and clicks to end/beginning of track
-def addLowPassFilter(audio_segment):
-    in_Wave = AudioSegment.from_wav(IN_WAV)-15 # reduce volume
-    out_Wave = AudioSegment.from_wav(OUT_WAV)-15 # reduce volume
-    filtered =  audio_segment.high_pass_filter(3000).low_pass_filter(4000)  # high and low pass filter, to compress the sound.
-    filtered = filtered + 10 # boost volume
-    filtered = strip_silence(filtered) # strip leading/trailing silence
-    combined =  in_Wave + filtered + out_Wave # concatenate
-    whiteNoise = generators.WhiteNoise().to_audio_segment(duration=len(combined)) # white noise profile generated
-    overlayed = combined.overlay(whiteNoise-25) # overlay white noise, dropping WN volume. 
-    overlayed = overlayed + 10 # bump volume of all audio 
+
+# Adds a couple filters, and adds static, and clicks to end/beginning of track
+def addRadioNoiseFilters(audio_segment):
+    in_Wave = AudioSegment.from_wav(_IN_WAV)-15  # reduce volume
+    out_Wave = AudioSegment.from_wav(_OUT_WAV)-15  # reduce volume
+    # high and low pass filter, to compress the sound.
+    filtered = audio_segment.high_pass_filter(3000).low_pass_filter(4000)
+    filtered = filtered + 10  # boost volume
+    filtered = _strip_silence(filtered)  # strip leading/trailing silence
+    combined = in_Wave + filtered + out_Wave  # concatenate
+    whiteNoise = generators.WhiteNoise().to_audio_segment(
+        duration=len(combined))  # white noise profile generated
+    # overlay white noise, dropping WN volume.
+    overlayed = combined.overlay(whiteNoise-25)
+    overlayed = overlayed + 10  # bump volume of all audio
     return overlayed
 
-## Converts from MP3 format to OGG for DCS Missions
+# Converts from MP3 format to OGG for DCS Missions
+
+
 def convertMP3toOGG(mp3FilenamePath):
     print("Converting "+mp3FilenamePath)
-    oggFileNamePath = mp3FilenamePath.replace(".wav",".ogg")
-    segment = AudioSegment.from_mp3(mp3FilenamePath) # it's a wave, but this works.
-    segment = addLowPassFilter(segment)
+    oggFileNamePath = mp3FilenamePath.replace(".wav", ".ogg")
+    # it's a wave, but this works.
+    segment = AudioSegment.from_mp3(mp3FilenamePath)
+    segment = addRadioNoiseFilters(segment)
     segment.export(oggFileNamePath, format='ogg')
     os.remove(mp3FilenamePath)
     return oggFileNamePath
 
-## Writes speech audio to MP3 format.
+# Writes speech audio to MP3 format.
+
+
 def writeSpeechToFile(voiceFileName, voiceText, voiceID, speechEngine):
-    speechEngine.setProperty('voice',voiceID)
+    speechEngine.setProperty('voice', voiceID)
     username = getpass.getuser()
     print(voiceFileName+" with audio: "+voiceText)
-    filePath = "C:\\Users\\"+ username +"\\Desktop\\"+ voiceFileName +".wav"
-    speechEngine.save_to_file(voiceText,filePath)
+    filePath = "C:\\Users\\" + username + "\\Desktop\\" + voiceFileName + ".wav"
+    speechEngine.save_to_file(voiceText, filePath)
     speechEngine.runAndWait()
     return filePath
 
-## Parses the CSV file argument into a dictionary for text.
+# Parses the CSV file argument into a dictionary for text.
+
+
 def parseLinesFromCSV(csvFilePath):
     lines = {}
-    for line in open(csvFilePath,'r').readlines():
+    for line in open(csvFilePath, 'r').readlines():
         splitParts = line.split(',')
-        lines[splitParts[0]]=splitParts[1]
+        lines[splitParts[0]] = splitParts[1]
     return lines
-    
+
+# List out all the voices on the system by ID
+
 
 def enumerateInstalledVoices(speechEngine):
     voices = speechEngine.getProperty('voices')
@@ -69,31 +88,44 @@ def enumerateInstalledVoices(speechEngine):
     print("You can copy/paste the Voice name from this console view into the code to set the desired voice.")
     input("Press <ENTER> to continue")
 
+# An object just to hold arguments in a namespace.
+
+
 class MyArgs:
     pass
 
+# parse command line arguments
 
-if __name__=='__main__':
-    givenArgs = MyArgs();
-    parser = argparse.ArgumentParser(description="A voice line generator, for generating radio-comms-like audio files for use with the DCS World Mission Editor.")
+
+def parseMyArgs(sysArgsv):
+    givenArgs = MyArgs()
+    parser = argparse.ArgumentParser(
+        description="A voice line generator, for generating radio-comms-like audio files for use with the DCS World Mission Editor.")
     parser.add_argument('--file', help="The absolute or relative path to a CSV file with your desired voice lines typed out in the correct format.",
-                        type=str,default=argparse.SUPPRESS)
-    parser.add_argument('--voice',help='Copy/Pasted value for Voice as displayed by \'--lsvoices\' flag',
-                        type=str,default=default_voiceID)
-    parser.add_argument('--lsvoices',help='list installed voice IDs',action='store_true')
-    
-    parser.parse_known_args(sys.argv,namespace=givenArgs)
-    print(givenArgs)
+                        type=str, default=argparse.SUPPRESS)
+    parser.add_argument('--voice', help='Copy/Pasted value for Voice as displayed by \'--lsvoices\' flag',
+                        type=str, default=_default_voiceID)
+    parser.add_argument(
+        '--lsvoices', help='list installed voice IDs', action='store_true')
+    parser.parse_known_args(sysArgsv, namespace=givenArgs)
+    return givenArgs
+
+
+if __name__ == '__main__':
+    givenArgs = parseMyArgs(sys.argv)
+
     engine = pyttsx3.init()
+
     if givenArgs.lsvoices == True:
         enumerateInstalledVoices(engine)
-    elif givenArgs.file != "" :
+    elif givenArgs.file != "":
         textToOutput = parseLinesFromCSV(givenArgs.file)
 
         print(textToOutput)
         for audioKey in textToOutput.keys():
-            filePath = writeSpeechToFile(audioKey, textToOutput[audioKey], givenArgs.voice, engine);
+            filePath = writeSpeechToFile(
+                audioKey, textToOutput[audioKey], givenArgs.voice, engine)
             oggFile = convertMP3toOGG(filePath)
             print("Created file: "+oggFile)
-    
+
     engine.stop()
